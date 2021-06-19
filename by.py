1a89aeedf90e4603421a3105a88bf0f42f624089
unknown <miika.kananen@gmail.com>
2021-06-19 22:36:25 +0900

    toinencommit

--- by.py
+++ by.py
@@ -1,16 +1,19 @@
 import discord
 from google.cloud import translate_v2 as translate
 import os
+import signal
+import sys
+
 
 # Credentials in order to use the Google Cloud Translator API
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r""
 
 # Translator object
 translate_client = translate.Client()
-
+lang = translate_client.get_languages()
+lang = [code["language"] for code in lang]
 # Initiate Discord client
 client = discord.Client()
-
 
 # Console message to let the user know bot is now online
 @client.event
@@ -28,14 +31,37 @@ async def on_message(message):
     if message.content.startswith("!bunyeok"):
         await message.channel.send("https://cloud.google.com/translate/docs/languages")
         await message.channel.send("!(targetLanguage) (message)\nex: !fr hello")
+        return
 
     # Translate command
-    if message.content.lower().startswith("!"):
-        msg = message.content[4:]
-        trg = message.content[1:3]
+    if message.content.startswith("!"):
+        content = message.content[1:].split(" ", 1)
+
+        try:
+            trg = content[0]
+        except IndexError:
+            await message.channel.send("Missing language code!")
+            return
+
+        if len(trg) != 2 or trg not in lang:
+            await message.channel.send("Invalid language code!")
+            return
+
+        try:
+            msg = content[1]
+        except IndexError:
+            await message.channel.send("Message missing!")
+            return
+
         translate_client.detect_language(msg)
         translated_message = translate_client.translate(msg, target_language=f"{trg}")
         await message.channel.send(translated_message["translatedText"])
+
+def signal_handler(_sig, _frame):
+    print("Exiting...")
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 # Bot token
 client.run("")
